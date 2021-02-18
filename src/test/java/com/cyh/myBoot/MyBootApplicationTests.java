334c222aadbae2f76925f1e0fdc59054b7da9ceb
ppassion <chenyanhui1087@163.com>
2021-02-18 13:26:03 +0800

    yaml

--- src/test/java/com/cyh/myBoot/MyBootApplicationTests.java
+++ src/test/java/com/cyh/myBoot/MyBootApplicationTests.java
@@ -1,13 +1,19 @@
 package com.cyh.myBoot;
 
+import com.cyh.myBoot.test.YamlTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MyBootApplicationTests {
 
+	@Autowired
+	private YamlTest yamlTest;
+
 	@Test
 	void contextLoads() {
+		System.out.println(yamlTest);
 	}
 
 }
